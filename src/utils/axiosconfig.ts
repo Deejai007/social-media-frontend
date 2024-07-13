77db98d5f4e509ed307fe66e6a3e257653b616e3
Deepak Jain <deepakjain2k19@gmail.com>
2024-07-13 12:50:52 +0530

    dep config

--- src/utils/axiosconfig.ts
+++ src/utils/axiosconfig.ts
@@ -1,15 +1,18 @@
 import axios from "axios";
+import { env } from "process";
 
 axios.defaults.withCredentials = true;
 
+console.log(import.meta.env.VITE_SERVER_URL);
 const axiosApi = axios.create({
-  baseURL: "http://localhost:8967",
+  baseURL: import.meta.env.VITE_SERVER_URL,
+  // baseURL: "http://localhost:8967",
   // timeout: 6000,
   headers: {
     "Content-Type": "application/json",
   },
 
-  // withCredentials: true, // Ensures that cookies are sent with every request
+  withCredentials: true, // Ensures that cookies are sent with every request
 });
 
 export default axiosApi;
